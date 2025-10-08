80e16c5d1ec8b72640bf73288b110c9e48582605
tanuu007 <141906352+tanuu007@users.noreply.github.com>
2025-10-08 15:52:36 +0530

    Added Recents Opened Cheats for playstation and xbox

--- lib/Presentation/Cheat_Screens/search.dart
+++ lib/Presentation/Cheat_Screens/search.dart
@@ -5,7 +5,7 @@ import 'package:all_gta/Models/cheat_cards.dart';
 import 'package:all_gta/Models/theme_colors.dart';
 import 'package:all_gta/Utils/code_mapper.dart';
 import 'package:all_gta/Models/image_swiper.dart';
-
+import 'package:all_gta/l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
@@ -24,25 +24,37 @@ class _SearchScreenState extends State<SearchScreen> {
 
   List<CheatCode> _allCheats = [];
   List<CheatCode> _filteredCheats = [];
-  List<Map<String, dynamic>> _recentCheats = [];
   bool _isLoading = true;
   String _searchQuery = '';
+  List<CheatCode> _recentCheats = [];
+
+  Future<void> _loadRecentCheats() async {
+    final prefs = await SharedPreferences.getInstance();
+    final stored = prefs.getString('recentCheats');
+    if (stored != null) {
+      final List<dynamic> decoded = jsonDecode(stored);
+      setState(() {
+        _recentCheats = decoded
+            .map((data) {
+              return CheatCode(
+                title: data['title'] ?? '',
+                description: data['description'] ?? '',
+                codes: data['codes'] ?? '',
+                section: data['section'] ?? '',
+                rawData: data,
+              );
+            })
+            .where((c) => c.rawData['platform'] == widget.platform)
+            .toList();
+      });
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     _loadCheats();
     _loadRecentCheats();
-  }
-
-  Future<void> _loadRecentCheats() async {
-    final prefs = await SharedPreferences.getInstance();
-    final list = prefs.getStringList('recentCheats') ?? [];
-    setState(() {
-      _recentCheats = list
-          .map((e) => jsonDecode(e) as Map<String, dynamic>)
-          .toList();
-    });
   }
 
   Future<void> _loadCheats() async {
@@ -60,6 +72,10 @@ class _SearchScreenState extends State<SearchScreen> {
         break;
       case 'pc':
         cheats = await CheatService.fetchPcCheats();
+        break;
+      case 'phone':
+      case 'phonenumbers':
+        cheats = await CheatService.fetchPhoneNumCheats();
         break;
     }
     setState(() {
@@ -86,25 +102,12 @@ class _SearchScreenState extends State<SearchScreen> {
     });
   }
 
-  void _openCheat(Map<String, dynamic> cheatData) async {
-    final cheat = CheatCode(
-      title: cheatData['title'],
-      description: cheatData['description'],
-      codes: cheatData['codes'],
-      section: '',
-      rawData: {},
-    );
+  _showBottomSheetWithImages(CheatCode cheat) {
+    final codes = cheat.codes.split(',').map((code) => code.trim()).toList();
 
-    String platform = cheatData['platform'];
-    Function(String)? mapper;
-    if (platform == 'xbox') mapper = getXboxImagePath;
-    if (platform == 'playstation') mapper = getPlaystationImagePath;
-
-    final codes = cheat.codes.split(',').map((e) => e.trim()).toList();
-    final List<String> imagePaths = mapper != null
-        ? codes.map((e) => mapper!(e).toString()).toList()
-        : <String>[];
-
+    final imagePaths = codes
+        .map((code) => getPlaystationImagePath(code))
+        .toList();
     final codeTexts = codes;
 
     showModalBottomSheet(
@@ -136,20 +139,44 @@ class _SearchScreenState extends State<SearchScreen> {
                     controller: _searchController,
                     focusNode: _searchFocusNode,
                     onChanged: _onSearchChanged,
+                    keyboardAppearance: Brightness.dark,
                     decoration: InputDecoration(
-                      hintText: 'Search cheats',
+                      hintText: AppLocalizations.of(context)!.searchText,
+                      hintStyle: const TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400,
+                        color: Color.fromRGBO(200, 196, 196, 1),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        color: Color.fromRGBO(200, 196, 196, 1),
+                        size: 35,
+                      ),
                       filled: true,
                       fillColor: AppColors.notSelectedbg,
-                      prefixIcon: const Icon(Icons.search, color: Colors.grey),
-                      border: OutlineInputBorder(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 14,
+                        horizontal: 20,
+                      ),
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide.none,
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(255, 255, 255, 0.1),
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(255, 255, 255, 0.1),
+                          width: 2,
+                        ),
                       ),
                     ),
                     style: const TextStyle(color: Colors.white),
                   ),
                 ),
-                if (_searchQuery.isNotEmpty)
+                if (_searchFocusNode.hasFocus || _searchQuery.isNotEmpty)
                   TextButton(
                     onPressed: () {
                       _searchController.clear();
@@ -165,89 +192,125 @@ class _SearchScreenState extends State<SearchScreen> {
             ),
             const SizedBox(height: 16),
 
+            // Center text or cheat cards
             Expanded(
               child: _isLoading
                   ? const Center(child: CircularProgressIndicator())
-                  : _searchQuery.isNotEmpty
-                  ? _buildSearchResults()
-                  : _buildRecentSection(),
+                  : _searchQuery.isEmpty
+                  ? (_recentCheats.isEmpty
+                        ? const Center(
+                            child: Text(
+                              'Search any cheats here',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                        : ListView(
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(bottom: 12),
+                                child: Text(
+                                  'Recently Viewed',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                              ..._recentCheats.map((cheat) {
+                                bool useImages =
+                                    widget.platform.toLowerCase() ==
+                                        'playstation' ||
+                                    widget.platform.toLowerCase() == 'xbox';
+
+                                String Function(String)? imageMapper;
+                                if (widget.platform.toLowerCase() ==
+                                    'playstation') {
+                                  imageMapper = getPlaystationImagePath;
+                                } else if (widget.platform.toLowerCase() ==
+                                    'xbox') {
+                                  imageMapper = getXboxImagePath;
+                                }
+
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 8,
+                                  ),
+                                  child: CheatCard(
+                                    title: cheat.title,
+                                    desc: cheat.description,
+                                    phoneNum: cheat.phoneNum,
+                                    buttons: cheat.codes
+                                        .split(',')
+                                        .map((b) => b.trim())
+                                        .toList(),
+                                    isFavorite: false,
+                                    onFavoriteToggle: (_) {},
+                                    useImages: useImages,
+                                    imageMapper: imageMapper,
+                                    onTap: useImages
+                                        ? () =>
+                                              _showBottomSheetWithImages(cheat)
+                                        : null,
+                                  ),
+                                );
+                              }).toList(),
+                            ],
+                          ))
+                  : _filteredCheats.isEmpty
+                  ? Center(
+                      child: Text(
+                        'No cheats found for "$_searchQuery"',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: _filteredCheats.length,
+                      itemBuilder: (context, index) {
+                        final cheat = _filteredCheats[index];
+                        bool useImages =
+                            widget.platform.toLowerCase() == 'playstation' ||
+                            widget.platform.toLowerCase() == 'xbox';
+
+                        String Function(String)? imageMapper;
+                        if (widget.platform.toLowerCase() == 'playstation') {
+                          imageMapper = getPlaystationImagePath;
+                        } else if (widget.platform.toLowerCase() == 'xbox') {
+                          imageMapper = getXboxImagePath;
+                        }
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: CheatCard(
+                            title: cheat.title,
+                            desc: cheat.description,
+                            phoneNum: cheat.phoneNum,
+                            buttons: cheat.codes
+                                .split(',')
+                                .map((b) => b.trim())
+                                .toList(),
+                            isFavorite: false,
+                            onFavoriteToggle: (_) {},
+                            useImages: useImages,
+                            imageMapper: imageMapper,
+                            onTap: useImages
+                                ? () => _showBottomSheetWithImages(cheat)
+                                : null,
+                          ),
+                        );
+                      },
+                    ),
             ),
           ],
         ),
       ),
-    );
-  }
-
-  Widget _buildSearchResults() {
-    if (_filteredCheats.isEmpty) {
-      return Center(
-        child: Text(
-          'No cheats found for "$_searchQuery"',
-          style: TextStyle(color: Colors.grey),
-        ),
-      );
-    }
-    return ListView.builder(
-      itemCount: _filteredCheats.length,
-      itemBuilder: (context, index) {
-        final cheat = _filteredCheats[index];
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: CheatCard(
-            title: cheat.title,
-            desc: cheat.description,
-            buttons: cheat.codes.split(',').map((e) => e.trim()).toList(),
-            isFavorite: false,
-            onFavoriteToggle: (_) {},
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildRecentSection() {
-    if (_recentCheats.isEmpty) {
-      return const Center(
-        child: Text(
-          'Search any cheats here',
-          style: TextStyle(color: Colors.grey),
-        ),
-      );
-    }
-
-    return ListView(
-      children: [
-        const Padding(
-          padding: EdgeInsets.only(bottom: 12, left: 4),
-          child: Text(
-            'Recent',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ),
-        ..._recentCheats.map((data) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: CheatCard(
-              title: data['title'],
-              desc: data['description'],
-              buttons: data['codes'].split(',').map((b) => b.trim()).toList(),
-              isFavorite: false,
-              onFavoriteToggle: (_) {},
-              onTap: () => _openCheat(data),
-              useImages: ['xbox', 'playstation'].contains(data['platform']),
-              imageMapper: data['platform'] == 'xbox'
-                  ? getXboxImagePath
-                  : data['platform'] == 'playstation'
-                  ? getPlaystationImagePath
-                  : null,
-            ),
-          );
-        }).toList(),
-      ],
     );
   }
 }
